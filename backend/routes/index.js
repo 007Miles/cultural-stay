05a48d63117458941cadc4517b7eb7b917bc58ec
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-05-02 23:26:04 +0530

    tourist attraction crud implemented

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import sampleRouter from './sampleRouter.js'
+import TouristAttractionRouter from './TouristAttraction/TouristAttraction.js'
 
 const router = express.Router()
 
 router.use('/sample', sampleRouter)
+router.use('/TASites', TouristAttractionRouter)
 
 export default router
