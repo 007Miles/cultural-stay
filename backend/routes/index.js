d9be959fe47eb835678fd1e30e62a0306e7534b4
it21055294 <it21055294@my.sliit.lk>
2023-05-03 12:22:17 +0530

    remove others and add models

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,9 @@
 import express from 'express'
 
 import sampleRouter from './sampleRouter.js'
-import hostRouter from './Host/host.js'
-import accommodationRouter from './Host/accommodation.js'
 
 const router = express.Router()
 
 router.use('/sample', sampleRouter)
-router.use('/host', hostRouter)
-router.use('/accommodation', accommodationRouter)
 
 export default router
