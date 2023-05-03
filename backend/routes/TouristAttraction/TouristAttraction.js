1ecf8b8da0f49241787c7348ec444219aad02a4b
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-05-03 21:46:58 +0530

    Navbar and color gradiant added to pages

--- backend/routes/TouristAttraction/TouristAttraction.js
+++ backend/routes/TouristAttraction/TouristAttraction.js
@@ -10,10 +10,16 @@ import {
   getTouristAttractionById,
 } from '../../controller/touristAttractionController.js'
 
+//get touristAttractions by search quary
 router.get('/', getAllTouristAttractions)
+
+//create new tourist attraction site
 router.post('/', createTouristAttraction)
+
 router.put('/:id', updateTouristAttraction)
+
 router.delete('/:id', deleteTouristAttraction)
+
 router.get('/:id', getTouristAttractionById)
 
 export default router
---
+++ frontend/src/routes/index.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from '../components/Navbar/Navbar'
+import Home from '../pages/Home/Home'
+
+const IndexRoutes = () => {
+  return (
+    <div>
+      <div class="bg-gradient-to-t from-[#c2fbd3] to-[#a6c1ee] h-screen w-screen">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </div>
+  )
+}
+
+export default IndexRoutes
