0ae6de383c6d4069190d80805dfcc872d0cbe356
Kariyawasam K.P.W.D.V <87995561+IT21036620@users.noreply.github.com>
2023-05-06 01:17:42 +0530

    Tourist Attraction Suggestion CRUD backend added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import sampleRouter from './sampleRouter.js'
 import foodRouter from './food/food.js'
 import restaurantRouter from './restaurants/restaurants.js'
 import TouristAttractionRouter from './TouristAttraction/TouristAttraction.js'
+import suggestionRouter from './TouristAttraction/suggestions.js'
 
 const router = express.Router()
 
@@ -11,5 +12,6 @@ router.use('/sample', sampleRouter)
 router.use('/food', foodRouter)
 router.use('/restaurants', restaurantRouter)
 router.use('/TASites', TouristAttractionRouter)
+router.use('/suggestion', suggestionRouter)
 
 export default router
