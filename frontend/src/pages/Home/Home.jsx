ff81525d9601f871c3047860a11561a2e076da7f
IT21055058 <it21055058@my.sliit.lk>
2023-05-06 13:02:15 +0530

    frontend initialized

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import Video from '../../Assets/v1.mp4'
-import './home.css'
-
+// import Video from '../../Assets/v1.mp4'
+// import './home.css'
 
 export const Home = () => {
   return (
-    <section className='home'>
-        <div className='overlay'>
-            <video src={Video} muted autoPlay loop type="video/mp4" className='h-full w-full relative'></video>
+    <section className="home">
+      <div className="overlay">
+        {/* <video src={Video} muted autoPlay loop type="video/mp4" className='h-full w-full relative'></video> */}
         <div>
-            <div>
-                <span className=''>
-                    Visit Sri Lanka
-                </span>
-                <h1>Learn Our Culture</h1>
-            </div>
+          <div>
+            <span className="">Visit Sri Lanka</span>
+            <h1>Learn Our Culture</h1>
+          </div>
         </div>
-        </div>
+      </div>
     </section>
   )
 }
