609fb408e722616791b6e42c358f7181ac125019
IT21055058 <it21055058@my.sliit.lk>
2023-05-20 02:55:03 +0530

    admin dashboard authorization complete frontend

--- frontend/src/components/Dashboard/libs/helpers/navigation.js
+++ frontend/src/components/Dashboard/libs/helpers/navigation.js
@@ -63,13 +63,7 @@ export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
   {
     key: 'settings',
     label: 'Settings',
-    path: '/settings',
+    // path: '/settings',
     icon: <HiOutlineCog />,
-  },
-  {
-    key: 'support',
-    label: 'Help & Support',
-    path: '/support',
-    icon: <HiOutlineQuestionMarkCircle />,
   },
 ]
