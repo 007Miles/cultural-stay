58958fa6729e6642e7d231dbdbb45a4b0a133c3a
IT21055058 <it21055058@my.sliit.lk>
2023-05-19 12:28:17 +0530

    frontend work

--- frontend/src/components/FeedBack and Review/RatingBox.js
+++ frontend/src/components/FeedBack and Review/RatingBox.js
@@ -4,7 +4,7 @@ import useAuth from '../../hooks/useAuth'
 import axios from 'axios'
 
 export default function RatingBox({ id }) {
-  const [value, setValue] = useState(4)
+  const [value, setValue] = useState(0)
   const [comments, setComments] = useState([])
   const { auth } = useAuth()
   const [rating, setRating] = useState()
@@ -28,6 +28,32 @@ export default function RatingBox({ id }) {
     getComments()
   }, [comments])
 
+  async function handleUpdate() {
+    // alert(cid)
+    // const comment = {}
+
+    try {
+      const response = await axios.patch(
+        `http://localhost:4000/api/feedback/ratings/${id}`,
+        { rating: { val: value, postedBy: auth.user } }
+      )
+
+      if (response) {
+        alert('success')
+      }
+      //   setText('')
+      //   // const accessToken = response?.data?.accessToken
+      //   // const role = response?.data?.role
+      //   // console.log(role)
+      //   // setAuth({ user, pwd, role, accessToken })
+      //   // setUser('')
+      //   // setPwd('')
+      //   // navigate('/admin')
+    } catch (err) {
+      console.log(err.response.message)
+    }
+  }
+
   return (
     <div>
       {' '}
@@ -40,19 +66,27 @@ export default function RatingBox({ id }) {
           <div className="rounded-lg overflow-hidden  mb-5">
             <Rating
               size="large"
-              name="simple-controlled"
+              name="read-only"
               value={value}
-              precision={'0.2'}
+              precision={0.2}
+              readOnly
+            />
+            {/* <Rating
+              size="large"
+              name="simple-controlled"
+              defaultValue={value}
+              precision={0.2}
               onChange={(event, newValue) => {
                 setValue(newValue)
               }}
-            />
+            /> */}
           </div>
         </div>
         <div className="px-6 pt-2 pb-2 text-left">
           <button
             className="w-full p-2 bg-yellow-600 text-white font-semibold rounded hover:bg-yellow-700"
-            onClick={() => alert(rating.val)}
+            // onClick={() => handleUpdate()}
+            onClick={() => alert(value)}
           >
             Rate
           </button>
